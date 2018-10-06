f0f840caf23ce2e5f8de933f6d6a71ee442ee518
flaeteh <flaeteh@gmail.com>
2018-10-06 18:19:17 +0800

    Initial commit

---
+++ app/src/main/java/florence/teh/com/tehflorenceexam1/MainActivity.kt
@@ -0,0 +1,12 @@
+package florence.teh.com.tehflorenceexam1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
